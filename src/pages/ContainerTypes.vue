dffa6dbfb1d5acf14b6710e4092bcd76f086f030
Chad Collins <chadrcollins91@gmail.com>
2024-03-04 12:07:17 -0500

    Skeleton for container types

--- src/pages/ContainerTypes.vue
+++ src/pages/ContainerTypes.vue
@@ -1,10 +1,72 @@
 <script setup lang="ts">
 
 import Page from '@/pages/Page.vue'
+import PageSection from '@/components/PageSection.vue'
+import ScriptExample from '@/components/ScriptExample.vue'
+
+const rangeExamples = [
+
+]
+
+const pairExamples = [
+
+]
+
+const listExamples = [
+
+]
+
+const concatenationExamples = [
+
+]
+
+const sliceExamples = [
+
+]
 </script>
 
 <template>
   <Page title="Container Types">
+    <PageSection>
+      <template v-slot:left>
+        <h3>Ranges</h3>
+      </template>
+      <template v-slot:right>
+        <ScriptExample :scripts="rangeExamples" />
+      </template>
+    </PageSection>
+    <PageSection>
+      <template v-slot:left>
+        <h3>Pairs</h3>
+      </template>
+      <template v-slot:right>
+        <ScriptExample :scripts="pairExamples" />
+      </template>
+    </PageSection>
+    <PageSection>
+      <template v-slot:left>
+        <h3>Lists</h3>
+      </template>
+      <template v-slot:right>
+        <ScriptExample :scripts="listExamples" />
+      </template>
+    </PageSection>
+    <PageSection>
+      <template v-slot:left>
+        <h3>Concatenations</h3>
+      </template>
+      <template v-slot:right>
+        <ScriptExample :scripts="concatenationExamples" />
+      </template>
+    </PageSection>
+    <PageSection>
+      <template v-slot:left>
+        <h3>Slices</h3>
+      </template>
+      <template v-slot:right>
+        <ScriptExample :scripts="sliceExamples" />
+      </template>
+    </PageSection>
   </Page>
 </template>
 
