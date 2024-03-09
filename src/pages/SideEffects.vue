bafdee07b407996a92ffc7c0592e186ba5f31573
Chad Collins <chadrcollins91@gmail.com>
2024-03-09 09:47:28 -0500

    Detailing side effects being logged to console

--- src/pages/SideEffects.vue
+++ src/pages/SideEffects.vue
@@ -17,9 +17,13 @@ const examples = [
         <p>
           The final way to group code is with <span class="definition-word">Side Effects</span> by surrounding code in
           brackets, <code class="v-code">[]</code>. A side effect, in general, is a piece of functionality that
-          shouldn't
-          affect the primary activities of a program. Since every operation in Garnish results in a value, this grouping
-          allows your script to ignore the result of a subset of code.
+          shouldn't affect the primary activities of a program. Since every operation in Garnish results in a value,
+          this grouping allows your script to ignore the result of a subset of code.
+        </p>
+        <p>
+          The application executing can also intercept the side effect, either ignoring it entirely or process it like
+          this site does. If you open the console in your web browser's developer tools, you'll see the result of the
+          side effect logged.
         </p>
       </template>
       <template v-slot:right>
